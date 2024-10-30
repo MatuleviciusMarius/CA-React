ed0cffe12bb96e17499b1acc7fe8663681ed2072
Marius Matulevicius <marius.matulevicius@twoday.com>
2024-10-30 15:51:57 +0200

    fix errors

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,3 @@
-import { Paper } from "@mui/material";
 import Header from "../../components/Header/Header";
 import Heading from "../../components/Heading/Heading";
 import PageWrapper from "../../components/PageWrapper";
@@ -9,7 +8,7 @@ export default function LoginPage() {
   return (
     <PageWrapper>
       <div className={styles.absoluteWrapper}>
-        <Header />
+        <Header  />
       </div>
       <div className={styles.wrapper}>
         <Heading />
