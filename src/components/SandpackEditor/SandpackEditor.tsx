b32124cca4565aac00abad3bd488ddf7c90cbed3
Marius Matulevicius <marius.matulevicius@twoday.com>
2024-11-25 16:05:20 +0200

    changer file explorer bg color

--- src/components/SandpackEditor/SandpackEditor.tsx
+++ src/components/SandpackEditor/SandpackEditor.tsx
@@ -1,12 +1,13 @@
 import { SandpackCodeEditor, SandpackFileExplorer, SandpackLayout, SandpackPreview } from "@codesandbox/sandpack-react";
 import { useInjectStyles } from "./hooks/useInjextStyles";
+import styles from "./SandpackEditor.module.scss";
 const editorHeight = "60vh";
 
 const SandpackEditor = () => {
   useInjectStyles();
   return (
     <SandpackLayout style={{ height: editorHeight }}>
-      <SandpackFileExplorer autoHiddenFiles style={{ height: editorHeight }} />
+      <SandpackFileExplorer autoHiddenFiles className={styles.editor} style={{ height: editorHeight }} />
       <SandpackCodeEditor style={{ height: editorHeight }} showTabs showLineNumbers wrapContent closableTabs />
       <SandpackPreview showRestartButton showSandpackErrorOverlay showNavigator showOpenInCodeSandbox={false} style={{ height: editorHeight }} />
       {/* <SandpackConsole style={{ height: editorHeight }} /> */}
