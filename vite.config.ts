d7cf164c86c6d2b166e92d3e4d0e0fc3fd23a819
Robertas Ankudovicius <robertas.ankudovicius@twoday.com>
2024-12-04 00:38:39 +0200

    fixing vercel error

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
   resolve: {
-    alias: {
-      "@router": "/src/router",
-      extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json"],
-    },
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json"],
   },
 });
