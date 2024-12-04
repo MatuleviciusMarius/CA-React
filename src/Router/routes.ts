da22d731044611cae00c5d47d03db745c279c60e
Robertas Ankudovicius <robertas.ankudovicius@twoday.com>
2024-12-04 09:15:56 +0200

    fixing routes

--- src/Router/routes.ts
+++ src/Router/routes.ts
@@ -1,8 +1,8 @@
-export class RoutePaths {
-  public static main = "/";
-  public static login = "/login";
-  public static register = "/register";
-  public static course = "/course/:id";
-  public static task = "/lesson/:id";
-  public static notFound = "*";
-}
+export const RoutePaths = {
+  main: "/",
+  login: "/login",
+  register: "/register",
+  course: "/course/:id",
+  task: "/lesson/:id",
+  notFound: "*",
+};
